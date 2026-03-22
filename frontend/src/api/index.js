36deb43623e679adec7948b1d9179711fcac2082
JoanRipollAbad <joanripoll210@gmail.com>
2026-03-22 20:22:09 +0100

    baseURL cambiada para la web

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost/api',
+  //baseURL: import.meta.env.VITE_API_URL || 'http://localhost/api',
+  baseURL: 'http://50.19.246.230/api',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
